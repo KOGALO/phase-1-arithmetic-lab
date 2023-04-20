1c8ea6e98103030731f7d8645e859015376e6409
kepherogalo <kepher.ogalo@student.moringaschool.com>
2023-04-20 10:58:44 -0700

    completed lad

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() *100);
+
+let num3 = 72;
+let num4 = 17;
+let mod = (num3 % num4);
+
+let max = Math.max(5,10,15,20);
+
+
+
+
+
+
+
+
+
